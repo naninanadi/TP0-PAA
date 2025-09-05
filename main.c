79a395e6a92171c7df8b0c099319f1aa2458c4a1
Ana Clara <nanakkj.sillaby@gmail.com>
2025-09-05 19:36:33 -0300

    limpando a main

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "menu.h"
-#define numlinhas 18
-#define numcolunas 78
+
 
 int main(){
 
@@ -9,20 +8,5 @@ int main(){
 
     decisao();
 
-    // char** teste = malloc(numlinhas * sizeof(char*));
-    // for (int i = 0; i < numlinhas; i++)
-    //     teste[i] = malloc(numcolunas * sizeof(char));
-
-    // Coordenadas cord;
-
-    // LimpaQuadro(teste,numlinhas,numcolunas);
-
-    // for(int i = 0; i < 10; i++){
-    //     cord = GeraCoordenadaSoma(teste);
-    //     EscreveSoma(teste, cord);
-    // }
-
-
-    // ImprimeQuadro(teste);
     return 0;
 }
